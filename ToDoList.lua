e85a5fe43042eef8f4f5249ef3d7e16552dbbbe6
Davis24 <davisma24@vcu.edu>
2018-07-09 15:38:34 -0400

    renamed list to task, added new methods to storing task objects

--- ToDoList.lua
+++ ToDoList.lua
@@ -1,10 +1,11 @@
-
 characterTable = {}
+reoccuringTaskTable = {}
+taskTable = {}
 
 --Meta Class
-listItem = {description = '', character = '', complete = false}
+task = {description = '', character = '', complete = false}
 
-function listItem:new(o,des,char)
+function task:new(o,des,char)
    o = o or {}
    setmetatable(o, self)
    self.__index = self
@@ -13,37 +14,51 @@ function listItem:new(o,des,char)
    return o
 end
 
-
 -------------------------
 --  Description Functions
 -------------------------
-
-function listItem:printDescription()
+function task:printDescription()
    print("Description ",self.description)
 end
 
-function listItem:returnDescription()
+function task:returnDescription()
     return self.description
 end
 
-function listItem:changeDescription(str)
+function task:changeDescription(str)
     self.description = str
 end
 
 -----------------------------
 --  Character Functions
 -----------------------------
-function listItem:printCharacter()
-    print("Character ",self.character)
+function task:printCharacter()
+    print("Character ", self.character)
 end
 
-function listItem:returnCharacter()
+function task:returnCharacter()
     return self.character
 end
 
-function listItem:changeCharacter(str)
+function task:changeCharacter(str)
     self.character = str
 end
+
+-----------------------------
+--  Complete Functions
+-----------------------------
+function task:returnComplete()
+    return self.complete
+end
+
+function task:setComplete(str)
+    self.complete = str
+end
+
+function task:printComplete()
+    print("Complete:", self.complete)
+end
+
 
 --[[
 function listFindCharacter()
@@ -59,21 +74,75 @@ end
 --]]
 
 --Derived Class method new
-reoccuringListItem = listItem:new()
+reoccuringTask = task:new()
 
-function reoccuringListItem:new(o,des,char,time)
-    o = o or listItem:new(o,des,char)
+-----------------------------
+--  Time Functions
+-----------------------------
+function reoccuringTask:new(o,des,char,time)
+    o = o or task:new(o,des,char)
     setmetatable(o, self)
     self.__index = self
     self.time = time
     return o
 end
 
-function reoccuringListItem:printTime()
+function reoccuringTask:printTime()
     print("Time: ", self.time)
 end
 
-myItem = reoccuringListItem:new(nil, "Testing", "Devy","12")
+function reoccuringTask:returnTime()
+    return self.time
+end
+
+function reoccuringTask:setTime(str)
+    self.time = str
+end
+
+myItem = reoccuringTask:new(nil, "Testing", "Devy","12")
+
+-- Insert(<table_name>, object)
+
+table.insert(reoccuringTable, myItem)
+
+--print("My List Item Name: ",reoccuringTable[1]:printDescription)
+
+--Function that decides which object gets created
+function createTask(des,char,time)
+    des = des or "shouldn't occur" -- returns first value unless its nil or false
+    char = char or 0
+    time = time or 0
+
+    --Insert new task into the correct table
+    if(time == 0) then
+        newTask = task:new(des,char)
+        table.insert(taskTable, newTask)
+    else
+        newTask = reoccuringTask:new(des,char,time)
+        table.insert(reoccuringTaskTable, newTask)
+    end
+end
+
+function deleteTask(location, table)
+    if(table == "reoccuring") then
+        table.remove(location)
+    else
+        table.remove(location)
+    end
+end
+
+function updateTask(location,table,des,char,time)
+    des = des or "shouldn't occur" -- returns first value unless its nil or false
+    char = char or 0
+    time = time or 0
+
+    if(table == "reoccuring") then
+        --reoccuringTable[location]
+    else
+        --table.remove(location)
+    end
+end
+
 
 --- OnLoad add character to table
 --- characterTable[<element position>] = "element data"
